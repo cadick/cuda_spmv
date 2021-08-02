e0728fbbb5511760093a51df961164497830f958
Caro <36821094+cadick@users.noreply.github.com>
2021-08-02 02:13:27 +0200

    added files

---
+++ make_print.h
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include "cuComplex.h"
+#include <algorithm>
+
+// takes a sparse matrix stored as array and makes it csr (compressed sparse row)
+// a: stores values of the non-zero values of the matrix
+// ia: stores cumulative nuber of non-zero elements up to i'th row (excluding)
+// ja: stores column index of each element in the a vector
+// total_nnz: number of non zero values in the sparse matrix
+void makeCSR(cuDoubleComplex *matrix, int rows, int cols, cuDoubleComplex *a, int *ia, int *ja, int *total_nnz, int m) {
+    int nnz = 0;	            // number of non zero values
+	int nnz_per_row = 0;        // number of non zero values per row
+	ia[0] = 0;
+
+	for(int i = 0; i < (rows*cols*m); i++) {
+		if(cuCreal(matrix[i]) != 0 && cuCimag(matrix[i]) != 0) {
+			a[nnz] = matrix[i];	// store non zero value
+			ja[nnz] = i % cols;   // store corresponding col to value	
+			nnz ++;
+			nnz_per_row ++;		        // store number of non zero values per row
+		}
+		if(i%cols == (cols -1)) {	    // if we are at the end of a row
+            // stores cumulative number of non-zero elements at row number + 1 (this is a CSR convention)
+            // length of ia = number of rows + 1
+			ia[i/cols + 1] = ia[i/cols] + nnz_per_row;
+			nnz_per_row = 0;
+		}/*
+        if(i%(rows*cols) == (rows*cols-1)) { // if we are at the end of a matrix
+            nnz = 0;
+        }*/
+	}
+    *total_nnz += nnz;
+}
+
+// prints the arrays into which our sparse matrix was converted into
+void printCSR(cuDoubleComplex *a, int *ia, int *ja, int rows, int *total_nnz, int m) {
+    std::cout<< "Array A: [";
+    for(int i = 0; i < *total_nnz; i++) {
+		std::cout<< "(" << cuCreal(a[i]) << ", " << cuCimag(a[i]) <<")   ";
+	}
+    std::cout<< "]\n";
+
+	std::cout<< "Array IA: [";
+	for(int j = 0; j < (rows*m+1); j++) {
+		std::cout<< ia[j] << ", ";
+	}
+    std::cout<< "]\n";
+
+	std::cout<< "Array JA: [";
+	for(int k = 0; k < *total_nnz; k++) {
+		std::cout<< ja[k] << ", ";
+	}
+    std::cout<< "]\n";
+
+    std::cout<< "NNZ:" <<*total_nnz<< "\n";
+}
+
+// generate random int from 1 to 100
+int getRand() {
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist100(1, 100); // distribution in range [1, 100]
+
+    return dist100(rng);
+}
+template <class RandomAccessIterator, class URNG>
+
+// fill Matrices with complex values (ranged 0 to 100), row wise
+void fillMatrix(cuDoubleComplex* m, int size) {
+    for (int i = 0; i < size; i++) {
+        // 80% chance of zero value
+        if (getRand() > 20) {
+            m[i] = make_cuDoubleComplex(0, 0);
+        }
+        else {
+            m[i] = make_cuDoubleComplex(getRand(), getRand());
+        }
+    }
+}
+
+// fill Matrices with complex values (ranged 0 to 100), row wise, with k zeros per row
+void fillStructuredMatrix(cuDoubleComplex* m, int totalrows, int cols, int k) {
+    // fill matrices with nonzero values
+    for (int value = 0; value < totalrows*cols; value++) {
+        m[value] = make_cuDoubleComplex(getRand(), getRand());
+    }
+
+    // create vector with cols values
+    std::vector<int> a(cols, 0);
+    for(int count = 0; count < cols; count++) {
+        a.at(count) = count;
+    }
+    
+    // fill each row with k zero values
+    for (int i = 0; i < totalrows; i++) {
+        /* For linux PC: */
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::shuffle(a.begin(), a.end(),g);
+
+        /* For my Windows PC: 
+        std::random_shuffle(a.begin(), a.end());
+        */
+
+        // make k random values zero
+        for (int j = 0; j < k; j++) {
+            m[i*cols+a.at(j)] = make_cuDoubleComplex(0, 0);
+        }
+    }
+}
+
+void printMatrix(cuDoubleComplex* m, int size, int columns) {
+    for (int i = 0; i < size; i++) {
+        std::cout << "(" << cuCreal(m[i]) << ", " << cuCimag(m[i]) << ")   ";
+        if (i % columns == (columns - 1)) {
+            std::cout << "\n";
+        }
+    }
+    std::cout << "\n";
+}
+
+// print numMtx amount of matrices
+void printMatrices(cuDoubleComplex* m, int singleSize, int columns, int numMtx) {
+    std::cout << "MATRICES\n";
+    for (int i = 0; i < singleSize * numMtx; i++) {
+        std::cout << "(" << cuCreal(m[i]) << ", " << cuCimag(m[i]) << ")   ";
+        if (i % columns == (columns - 1)) {
+            std::cout << "\n";
+        }
+        if (i % singleSize == singleSize - 1) {
+            std::cout << "-----------------------------------------------------\n";
+        }
+    }
+    std::cout << "\n";
+}
+
+// fill Vectors (we fill v_y) with complex values from 0 to d_B
+void fillVector(cuDoubleComplex* v, int size) {
+    for (int i = 0; i < size; i++) {
+        v[i] = make_cuDoubleComplex(getRand(), getRand());
+    }
+}
+
+// must provide which vector should be printed and how many rows the vector has
+void printVector(cuDoubleComplex* v, int rows) {
+    for (int i = 0; i < rows; i++) {
+        std::cout << "(" << cuCreal(v[i]) << ", " << cuCimag(v[i]) << ")\n";
+    }
+    std::cout << "\n";
+}
+
+// print numV amount of vectors
+void printVectors(cuDoubleComplex* v, int rows, int numV) {
+    std::cout << "VECTORS\n";
+    for (int i = 0; i < rows * numV; i++) {
+        std::cout << "(" << cuCreal(v[i]) << ", " << cuCimag(v[i]) << ")\n";
+
+        if (i % rows == rows - 1) {
+            std::cout << "-------------\n";
+        }
+    }
+    std::cout << "\n";
+}
---
+++ resultgen.cu
@@ -0,0 +1,256 @@
+// final comparison: c++, parallel mv multiplication, sparse mv multiplication with fft
+
+// CentOS compiling:
+// export LD_LIBRARY_PATH="/usr/local/cuda-11.0/targets/x86_64-linux/lib/"
+// nvcc -std=c++11 resultgen.cu -o resultgen -I/usr/local/cuda/targets/x86_64-linux/include/ -L/usr/local/cuda/targets/x86_64-linux/lib/ -lcufft
+// ./resultgen
+
+// Windows compiling:
+// nvcc resultgen.cu -o resultgen -I/usr/local/cuda/targets/x86_64-linux/include/ -L/usr/local/cuda/targets/x86_64-linux/lib/ -lcufft
+// resultgen.exe
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <cufft.h>
+#include "make_print.h"
+#include "cuComplex.h"
+
+#define M 500        // number of Matrices we want to multiply
+#define V 10         // number of Vectors we want to multiply
+#define dimU 16      // number of senders = rows of Matrix h_m_W
+#define dimB 128     // number of receivers = columns of Matrix h_m_W
+#define K 12         // number of zero values per row
+#define MATRIX_SIZE dimU*dimB
+#define MATRIXW_BYTES MATRIX_SIZE*sizeof(cuDoubleComplex)
+#define VECTORY_BYTES dimB*sizeof(cuDoubleComplex)
+#define VECTORR_BYTES dimU*sizeof(cuDoubleComplex)
+
+dim3 getDimGrid(const int m, const int n) {
+    dim3 dimGrid(m, n, 1);
+    return dimGrid;
+}
+
+dim3 getDimBlock(const int m, const int n) {
+    dim3 dimBlock(m, n, 1);
+    return dimBlock;
+}
+
+/********************************** normal MV *********************************/
+
+// matrix/vector multiplication, one thread per row
+// cuDoubleComplex is complex number for cuda (cuFloatComplex does NOT work/exist)
+__global__ void matrixVectorMultiplication(cuDoubleComplex *d_m_W, cuDoubleComplex *d_v_y, cuDoubleComplex *d_v_r) {
+    // each thread represents one row in one of the Matrices
+    int threadId = ((gridDim.x * blockIdx.y + blockIdx.x) * blockDim.x * 1) + threadIdx.x;
+
+    cuDoubleComplex sum = make_cuDoubleComplex(0, 0);
+    
+    // each thread does one row of multiplications
+    for(int i = 0; i < dimB; i++) {
+        sum = cuCadd(sum, cuCmul(d_m_W[(threadId%(M*dimU))* dimB  + i], d_v_y[ i + (threadId / (M * dimU)) * dimB]));
+    }
+    d_v_r[threadId] = sum;
+
+}
+
+/********************************** normal MV *********************************/
+
+/************************************ SPMV ************************************/
+
+// matrix/vector multiplication, one thread per row
+// cuDoubleComplex is complex number for cuda (cuFloatComplex does NOT work/exist)
+__global__ void sparseMatrixVectorMultiplication(cuDoubleComplex* d_a, int* d_ia, int* d_ja, cuDoubleComplex *d_v_y, cuDoubleComplex* d_a_r) {
+    // each thread represents one row in one of the Matrices
+    int threadId = ((gridDim.x * blockIdx.y + blockIdx.x) * blockDim.x * 1) + threadIdx.x;
+
+    cuDoubleComplex sum = make_cuDoubleComplex(0, 0);
+
+    // each thread does one row of non zero multiplications
+    for (int i = d_ia[(threadId%(M*dimU))]; i < d_ia[(threadId%(M*dimU)) +1]; i++) {
+        sum = cuCadd(sum, cuCmul(d_a[i], d_v_y[d_ja[i]+(threadId / (M * dimU)) * dimB]));
+    }
+    d_a_r[threadId] = sum;
+}
+
+/************************************ SPMV ************************************/
+
+int main() {
+    for (int zeros = 0; zeros < 16; zeros++) {
+        for (int iteration = 0; iteration < 3; iteration++) {
+
+            // declare host matrices and vectors
+            cuDoubleComplex* h_m_W, * h_v_y, * h_v_r, * h_a_r, * h_a_rfft, * h_a;
+            int* h_ia, * h_ja;
+            int total_nnz = 0;
+
+            // allocate Memory
+            h_m_W = (cuDoubleComplex*)malloc(MATRIXW_BYTES * M);
+            h_v_y = (cuDoubleComplex*)malloc(VECTORY_BYTES * V);
+            h_v_r = (cuDoubleComplex*)malloc(VECTORR_BYTES * M * V);
+            h_a_r = (cuDoubleComplex*)malloc(VECTORR_BYTES * M * V);
+            h_a_rfft = (cuDoubleComplex*)malloc(VECTORR_BYTES * M * V);
+            h_a = (cuDoubleComplex*)malloc(MATRIXW_BYTES * M);
+            h_ia = (int*)malloc((dimU * M + 1) * sizeof(int));
+            h_ja = (int*)malloc(M * dimU * dimB * sizeof(int));
+
+            // declare GPU memory pointers
+            cuDoubleComplex* d_m_W, * d_v_y, * d_v_r, * d_a_r, * d_a_rfft, * d_a;
+            int* d_ia, * d_ja;
+
+            // filling matrices with rendom amount of zeros per row
+            //fillMatrix(h_m_W, MATRIX_SIZE * M);
+            //printMatrices(h_m_W, MATRIX_SIZE, dimB, M);
+
+
+
+            // fill matrices with k zeros per row
+            fillStructuredMatrix(h_m_W, dimU * M, dimB, zeros);
+            //printMatrices(h_m_W, MATRIX_SIZE, dimB, M);
+
+            fillVector(h_v_y, dimB * V);
+            //printVectors(h_v_y, dimB, V);
+
+            // make matrices CSR
+            makeCSR(h_m_W, dimU, dimB, h_a, h_ia, h_ja, &total_nnz, M);
+            //printCSR(h_a, h_ia, h_ja, dimU, &total_nnz, M);
+
+            // allocate GPU memory pointers
+            cudaMalloc((void**)&d_m_W, MATRIXW_BYTES * M);
+            cudaMalloc((void**)&d_v_y, VECTORY_BYTES * V);
+            cudaMalloc((void**)&d_v_r, VECTORR_BYTES * M * V);
+            cudaMalloc((void**)&d_a_r, VECTORR_BYTES * M * V);
+            cudaMalloc((void**)&d_a_rfft, VECTORR_BYTES * M * V);
+            cudaMalloc((void**)&d_a, total_nnz * sizeof(cuDoubleComplex));
+            cudaMalloc((void**)&d_ia, (dimU * M + 1) * sizeof(int));
+            cudaMalloc((void**)&d_ja, total_nnz * sizeof(int));
+
+            // calculate the necessary space (same fo MV and SPMV)
+            dim3 dimGrid = getDimGrid(M, V);
+            dim3 dimBlock = getDimBlock(dimU, 1);
+
+            /********************************** normal MV *********************************/
+
+            // transfer the array to the GPU
+            cudaMemcpy(d_m_W, h_m_W, M * MATRIXW_BYTES, cudaMemcpyHostToDevice);
+            cudaMemcpy(d_v_y, h_v_y, V * VECTORY_BYTES, cudaMemcpyHostToDevice);
+
+            auto start_mv = std::chrono::high_resolution_clock::now();
+
+            // launch the kernel
+            matrixVectorMultiplication << <dimGrid, dimBlock >> > (d_m_W, d_v_y, d_v_r);
+
+            auto finish_mv = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed_mv = finish_mv - start_mv;
+
+            // copy back the result array to the CPU
+            cudaMemcpy(h_v_r, d_v_r, M * V * VECTORR_BYTES, cudaMemcpyDeviceToHost);
+
+            // free GPU memory allocation
+            cudaFree(d_m_W);
+            cudaFree(d_v_y);
+            cudaFree(d_v_r);
+
+            /********************************** normal MV *********************************/
+            
+            cudaDeviceSynchronize();
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+
+            /************************************ SPMV ************************************/
+
+            //transfer the array to the GPU
+            cudaMemcpy(d_v_y, h_v_y, V * VECTORY_BYTES, cudaMemcpyHostToDevice);
+            cudaMemcpy(d_a, h_a, total_nnz * sizeof(cuDoubleComplex), cudaMemcpyHostToDevice);
+            cudaMemcpy(d_ia, h_ia, (dimU * M + 1) * sizeof(int), cudaMemcpyHostToDevice);
+            cudaMemcpy(d_ja, h_ja, total_nnz * sizeof(int), cudaMemcpyHostToDevice);
+            cudaMemcpy(d_a_r, h_a_r, VECTORR_BYTES * M * V, cudaMemcpyHostToDevice);
+
+            // create plan for FFT
+            cufftHandle plan;
+            cufftPlan1d(&plan, dimU, CUFFT_Z2Z, M * V);
+
+            auto start_spmv = std::chrono::high_resolution_clock::now();
+
+            // launch the kernel
+            sparseMatrixVectorMultiplication << <dimGrid, dimBlock >> > (d_a, d_ia, d_ja, d_v_y, d_a_r);
+            auto finish_spmv = std::chrono::high_resolution_clock::now();
+
+            // FFT
+            cufftExecZ2Z(plan, d_a_r, d_a_rfft, CUFFT_FORWARD);
+            auto finish_spmvfft = std::chrono::high_resolution_clock::now();
+
+            cufftDestroy(plan);
+
+            // copy back the result array to the CPU
+            cudaMemcpy(h_a_r, d_a_r, M * V * VECTORR_BYTES, cudaMemcpyDeviceToHost);
+            cudaMemcpy(h_a_rfft, d_a_rfft, VECTORR_BYTES * M * V, cudaMemcpyDeviceToHost);
+
+            //printVectors(h_a_r, dimU, M*V);
+            //printVectors(h_a_rfft, dimU, M*V);
+
+            std::chrono::duration<double> elapsed_spmv = finish_spmv - start_spmv;
+            std::chrono::duration<double> elapsed_spmvfft = finish_spmvfft - start_spmv;
+
+
+            // free GPU memory allocation
+            cudaFree(d_a_rfft);
+            cudaFree(d_a_r);
+            cudaFree(d_a);
+            cudaFree(d_ia);
+            cudaFree(d_ja);
+
+            /************************************ SPMV ************************************/
+            
+            cudaDeviceSynchronize();
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+
+            /********************************** c++ part **********************************
+
+            cuDoubleComplex* cppResult;
+            cppResult = (cuDoubleComplex*)malloc(M * V * VECTORR_BYTES);
+
+            auto start_cpp = std::chrono::high_resolution_clock::now();
+            // loops through the result Array, same memory access as cuda implementation
+            for (int i = 0; i < dimU * M * V; i++) {
+                cppResult[i] = make_cuDoubleComplex(0, 0);
+                for (int j = 0; j < dimB; j++) {
+                    cppResult[i] = cuCadd(cppResult[i], cuCmul(h_m_W[((i % (dimU * M)) * dimB) + j], h_v_y[((i / (dimU * M))) * dimB + j]));
+                }
+            }
+            auto finish_cpp = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed_cpp = finish_cpp - start_cpp;
+
+            ********************************** c++ part **********************************/
+            
+
+            std::cout << "\n\nRESULTS:\n\n";
+            //printVectors(a, dimU, V*M);
+            //printVectors(cppResult, dimU, V * M);
+
+            // Check if MV == SPMV result
+            int errorCount = 0;
+            for (int i = 0; i < dimU * M * V; i++) {
+                if (cuCreal(h_v_r[i]) == cuCreal(h_a_r[i]) && cuCimag(h_v_r[i]) == cuCimag(h_a_r[i])) {
+                    continue;
+                }
+                else {
+                    errorCount++;
+                }
+            }
+
+            if (errorCount == 0) {
+                std::cout << "SPMV results equal MV result \n";
+            }
+            else {
+                std::cout << "SPMV result not equal to MV, number of Errors: " << errorCount << " \n";
+            }
+
+            std::cout << "\nComputation time:\nMV:       " << elapsed_mv.count() << " s\nSPMV:     " << elapsed_spmv.count() << " s\nSPMV+FFT: " << elapsed_spmvfft.count() << " s\n";
+            //std::cout <<"c++:      " << elapsed_cpp.count() << " s\n";
+            std::cout << "rows: " << dimU << ", cols: " << dimB << ", matrices: " << M << ", vectors: " << V << ", zeros: " << zeros << "\n";
+
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+        }
+    }
+    return 0;
+}
